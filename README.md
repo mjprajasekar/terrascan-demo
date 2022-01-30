afca787e2c0ab724aae0d0669d701ddc99ad63b2
devbyaccident <github@charliebravo.33mail.com>
2022-01-30 12:07:26 -0500

    🎉 Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Risk Management Strategy with Terrascan
+
+[Terrascan documentation](https://runterrascan.io/)
+
+# Install Terrascan (Linux)
+```
+curl -L "$(curl -s https://api.github.com/repos/accurics/terrascan/releases/latest | grep -o -E "https://.+?_Linux_x86_64.tar.gz")" > terrascan.tar.gz
+tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
+install terrascan /usr/local/bin && rm terrascan
+terrascan
+```
+
+# Install Terrascan (MacOS)
+```
+curl -L "$(curl -s https://api.github.com/repos/accurics/terrascan/releases/latest | grep -o -E "https://.+?_Darwin_x86_64.tar.gz")" > terrascan.tar.gz
+tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
+install terrascan /usr/local/bin && rm terrascan
+terrascan
+```
---
+++ main.tf
@@ -0,0 +1,29 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_kms_key" "aws_s3_key" {
+  key_id = "alias/aws/s3"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket        = "demo-bucket-${data.aws_caller_identity.current.account_id}"
+  acl           = "private"
+  force_destroy = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = data.aws_kms_key.aws_s3_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.demo_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}

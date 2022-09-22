5d1ca52747202b670c24ce6c1335e96d9e301805
mjprajasekar <mjp.rajasekar@gmail.com>
2022-09-22 11:10:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ data "aws_kms_key" "aws_s3_key" {
 
 resource "aws_s3_bucket" "demo_bucket" {
   bucket        = "demo-bucket-${data.aws_caller_identity.current.account_id}"
-  acl           = "private"
-  force_destroy = true
+  acl           = "public"
+  force_destroy = flase
 
   server_side_encryption_configuration {
     rule {
